ba67864a9a32a9a5ce3a6acd57bd33b64cb85d54
MiekeSlim <51536401+MiekeSlim@users.noreply.github.com>
2020-10-13 20:49:50 +0200

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -63,7 +63,7 @@ PennController.Sequence("LexTale_instructions", "LexTale_trials", "closing")
     .log( "Stimulus"    , trial.Stimulus    )
     .log( "Type"        , trial.Type        )
     .log( "Block"       , trial.Block       )
-  //  .log( "ID" , getVar("ID") )
+    .log( "ID" , getVar("ID") )
     )
  
 // Send results to server
